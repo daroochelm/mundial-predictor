b07733be7248f3a4f2373175a41c10a46569834c
daroochelm <darooche@gmail.com>
2026-06-12 15:04:45 +0200

    returntoold

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -49,14 +49,7 @@ export default function Navbar() {
           >
             Ranking
           </Link>
-          <Link 
-            href="/prediction-board" 
-            className={`font-medium transition-colors ${
-              pathname === '/ranking' ? 'text-yellow-400' : 'text-slate-400 hover:text-slate-200'
-            }`}
-          >
-            Typy innych
-          </Link>
+         
         </div>
 
         {/* Przycisk wylogowania */}
