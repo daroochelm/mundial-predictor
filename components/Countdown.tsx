effc4fde803d9246a29a4718704bb3ca7197b56c
daroochelm <darooche@gmail.com>
2026-06-12 14:18:48 +0200

    countend

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -7,7 +7,7 @@ const WorldCupCountdown = () => {
   // Funkcja renderująca własny format
   const renderer = ({ days, hours, minutes, seconds, completed }: CountdownRenderProps) => {
     if (completed) {
-      return <span className="text-cyan-400 font-bold">World Cup 2026 już trwa!</span>;
+      return <span className="text-cyan-400 font-bold">World Cup 2026 już trwa!!!</span>;
     } else {
       return (
         <div className="flex justify-center gap-2 text-white font-mono">
@@ -22,7 +22,7 @@ const WorldCupCountdown = () => {
 
   return (
     <div className="text-center py-4 bg-slate-900/50 rounded-2xl border border-slate-700/50 my-4">
-      <h2 className="text-xs uppercase tracking-widest text-slate-400 mb-2 font-bold">Do rozpoczęcia World Cup 2026:</h2>
+      
       <div className="text-xl md:text-2xl font-bold">
         <Countdown date={wcDate} renderer={renderer} />
       </div>
