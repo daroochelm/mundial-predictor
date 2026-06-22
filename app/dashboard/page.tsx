95a83a378bc95ca67d5e2c7ac57d763085236a54
daroochelm <darooche@gmail.com>
2026-06-22 20:02:50 +0200

    missedp

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -16,7 +16,7 @@ export default function DashboardPage() {
 
   const renderEventIcon = (type: string, detail: string) => {
     // Jeśli to niewykorzystany rzut karny, zwróć inną ikonę lub nic
-    if (detail === 'Missed Penalty') return <span className="text-red-500">❌ Missed Penalty</span>;
+    if (detail === 'Missed Penalty') return <span className="text-red-500">❌</span>;
     
     if (type === 'Goal') return <span>⚽</span>;
     if (type === 'Card') return <span className={`w-2 h-4 rounded-sm inline-block ${detail === 'Yellow Card' ? 'bg-yellow-400' : 'bg-red-600'}`}></span>;
